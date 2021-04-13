434ffc25d80f1b43b34e79f3d254803d614ec598
BORNBEAN <69454668+BORNBEAN@users.noreply.github.com>
2021-04-13 22:19:30 +0900

    Create 001_if_14681.java

---
+++ bornbean/Baekjoon/001_if_14681.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+        int x = Integer.parseInt(bf.readLine());
+        int y = Integer.parseInt(bf.readLine());
+        
+        if(x>0)
+            if(y>0) System.out.println(1);
+            else System.out.println(4);
+        else
+            if(y>0) System.out.println(2);
+            else System.out.println(3);
+    }
+}
