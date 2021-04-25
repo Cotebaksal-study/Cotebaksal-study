114290c41c121e379b702d89d2c66e8a5710c8c7
dominRd <82224759+dominRd@users.noreply.github.com>
2021-04-25 22:53:23 +0900

    Update M007_4344.java

--- domin/백준/M007_4344.java
+++ domin/백준/M007_4344.java
@@ -1,49 +1,41 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.DecimalFormat;
+import java.util.StringTokenizer;
 
 public class Main {
-
-        public static void main(String[] args) throws Exception{
-              
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            
-            // 테스트 케이스 개수를 세는 c
-            int c = Integer.parseInt(br.readLine());
-            // 결과값 초기화
-            double result = 0;
-            
-            for(int i = 0 ; i < c ; i++){
-                // c번째 행
-                
-                // 합계
-                int sum = 0;
-                String st = br.readLine();
-                // 받은수 (9 10 20 30 40 50 60 70 80 90)
-                String[] arr = st.split(" ");
-                int n = Integer.parseInt(arr[0]);
-                for(int z = 1; z <= n; z++){
-                    int grade = Integer.parseInt(arr[z]);
-                    sum+= grade;
-                }
-                double avg = sum / n;
-                
-                // 평균이 넘는 학생의 수
-                double count = 0;
-                for(int j = 1; j <= n; j++) {
-                    int grade = Integer.parseInt(arr[j]);
-                    if(grade > avg) {
-                        count++;
-                    }
-                }
-                
-                // 학생수 9명중 평균이상의 학생은 5명이다.
-                result =  Math.round(((count / n) * 100) * (double)1000.0)/1000.0;
-                DecimalFormat form = new DecimalFormat("0.000");
-                System.out.println(form.format(result) + "%");
-
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                    
+        // 테스트 케이스 개수를 세는 c
+        int c = Integer.parseInt(br.readLine());
+                    
+        for(int i = 0 ; i < c ; i++){
+            int sum = 0;
+            int count = 0; 
+            double avg = 0;
+         
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken()); // 전체 학생 수
+            int[] arr = new int[n];                
+         
+            // 1. 성적 총 합계(sum)
+            for(int j = 0; j < n; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+                sum += arr[j];
             }
-            br.close();
-
+            
+            // 1-1. 평균
+            avg = sum / n;
+         
+            // 2. 평균초과하는 학생 수(count)
+            for(int k = 0; k < n; k++) {
+                if(arr[k] > avg) {
+                    count++;
+                }
+            }
+            System.out.printf("%.3f%%\n", (double)count / n * 100);
         }
+        br.close();
+    }
 }
