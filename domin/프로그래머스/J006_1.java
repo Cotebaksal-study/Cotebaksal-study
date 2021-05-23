8d0dac490615f4c12c8d1a52b049a00d4ab2976c
dominRd <82224759+dominRd@users.noreply.github.com>
2021-05-23 22:29:44 +0900

    Create J006_1.java

---
+++ domin/프로그래머스/J006_1.java
@@ -0,0 +1,12 @@
+//[약수의 합]
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        // 1 2 3 4 6 12
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0) answer += i;
+        }
+        return answer;
+    }
+}
