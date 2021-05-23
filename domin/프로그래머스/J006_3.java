c502f5a4f5812ed97f6f6b3de99c4ecf7511de3b
dominRd <82224759+dominRd@users.noreply.github.com>
2021-05-23 22:30:35 +0900

    Create J006_3.java

---
+++ domin/프로그래머스/J006_3.java
@@ -0,0 +1,20 @@
+// 예산
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);    
+
+        for(int i = 0; i < d.length; i++){
+            if(d[i] > budget){
+                continue;
+            }else{
+                answer++;
+                budget -= d[i];
+            }
+        }
+                
+        return answer;
+    }
+}
